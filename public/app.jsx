7279ae9fc1b3785cef710ccaab2a124cc011ccd6
Brix Angeles <brix.angeles@gmail.com>
2017-01-12 00:19:49 -0800

    added default prop for p

--- public/app.jsx
+++ public/app.jsx
@@ -1,21 +1,26 @@
 var Greeter = React.createClass({
   getDefaultProps: function(){
     return {
-      name: 'React'
+      name: 'React',
+      message: 'default message'
     };
   },
   render: function () {
     var name = this.props.name;
+    var message = this.props.message;
     return (
       <div>
         <h1>Hello {name}!</h1>
-          <p>This is from the component</p>
+      <p>{message + '!!!'}</p>
       </div>
     );
   }
 });
 
+var firstName = "Brix";
+var firstMessage = "React is fun!"
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message={firstMessage}/>,
   document.getElementById('app')
 );
